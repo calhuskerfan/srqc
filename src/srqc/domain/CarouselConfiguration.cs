71d0ae644e76f639c497148d9af76749341605ea
calhuskerfan <calhuskerfan@yahoo.com>
2025-04-11 15:31:30 -0700

    updates Update README.md Fixed formatting checkpoint

--- src/srqc/domain/CarouselConfiguration.cs
+++ src/srqc/domain/CarouselConfiguration.cs
@@ -4,5 +4,7 @@
     {
         public int PodCount { get; set; }
         public int BoardingQueueSize { get; set; } = 3;
+        public bool LogInvoke { get; set; } = false;
+        public bool SuppressNoisyINF { get; set; } = false;
     }
 }
